83a63cbac56e0e1ab795a23a88376174f8e0f762
Stefan-T <102226495+Stefan-T@users.noreply.github.com>
2022-03-24 00:11:02 +0900

    Add files via upload

--- Main.java
+++ Main.java
@@ -5,10 +5,12 @@ class Ludi {
     int Rost,Pol;
 }
 public class Main {
-    public static Ludi [] setLudi(int k)
-    {
+
+    public static void main(String[] args) {
         Scanner sc=new Scanner(System.in,"cp1251");
-        Ludi []ludi=new Ludi[k];
+        System.out.println("Введите количество людей=> ");
+        int n=sc.nextInt();
+        Ludi ludi[]=new Ludi[n];
         System.out.println("Введите информацию о людях: ");
         for (int i = 0; i < ludi.length; i++) {
             sc.nextLine();
@@ -28,94 +30,84 @@ public class Main {
                 ludi[i].Pol1="Женщина";
             }
         }
-        return ludi;
-    }
-    public static void showLudi(Ludi[] lud) {
-        for (int i = 0; i < lud.length; i++) {
-            System.out.println("" + lud[i].Fam + "\t" + lud[i].Name + "\t" + lud[i].Rost + "см" + "\t" + lud[i].Pol1);
+        System.out.println("\nХарактеристики людей:");
+        for (int i = 0; i < ludi.length; i++) {
+            System.out.println(""+ludi[i].Fam+"\t"+ludi[i].Name +"\t"+ludi[i].Rost+"см"+"\t"+ludi[i].Pol1);
         }
-    }
 
-    public static double avgRostM(Ludi[] lud)
-    {
         double s=0;
         int m=0;
-        for (int i = 0; i < lud.length; i++)
-            if (lud[i].Pol==1){
+        for (int i = 0; i < ludi.length; i++)
+            if (ludi[i].Pol==1){
                 m=m+1;
-                s+=lud[i].Rost;
+                s+=ludi[i].Rost;
             }
         double sr=s/m;
-        return (sr);
-    }
-    public static double avgRostZH(Ludi[] lud)
-    {
+        System.out.println("Средний рост мужчин ="+sr);
         double s1=0;
         int zh=0;
-        for (int i = 0; i < lud.length; i++)
-            if (lud[i].Pol==2){
+        for (int i = 0; i < ludi.length; i++)
+            if (ludi[i].Pol==2){
                 zh=zh+1;
-                s1+=lud[i].Rost;
+                s1+=ludi[i].Rost;
             }
         double sr1=s1/zh;
-        return (sr1);
-    }
-    public static int NomMax(Ludi[] lud)
-    {
+        System.out.println("Средний рост женщин ="+sr1);
+
         int nommax=0;
-        double max=lud[nommax].Rost;
-        for (int i = 0; i < lud.length; i++)
-            if (lud[i].Pol==1) {
-                if (lud[i].Rost>max) {
-                    max = lud[i].Rost;
+        double max=ludi[nommax].Rost;
+        for (int i = 0; i < ludi.length; i++)
+            if (ludi[i].Pol==1) {
+                if (ludi[i].Rost>max) {
+                    max = ludi[i].Rost;
                     nommax = i;
                 }
             }
-        return nommax;
-    }
-    public static void SortRost(Ludi[] lud)
-    {
+        System.out.println("\nСамый высокий мужчина :");
+        System.out.println(""+ludi[nommax].Fam+"\t"+ludi[nommax].Name+"\t"+ludi[nommax].Rost+" см");
 
-        for (int i = 0; i < lud.length-1; i++)
-            for (int j = 0; j < lud.length-1-i; j++)
-                if (lud[j].Rost>lud[j+1].Rost){
-                    Ludi rab=lud[j]; // rab – рабочая переменная для перестановки
-                    lud[j]=lud[j+1];
-                    lud[j+1]=rab;
+        for (int i = 0; i < ludi.length-1; i++)
+            for (int j = 0; j < ludi.length-1-i; j++)
+                if (ludi[j].Rost>ludi[j+1].Rost){
+                    Ludi rab=ludi[j]; // rab – рабочая переменная для перестановки
+                    ludi[j]=ludi[j+1];
+                    ludi[j+1]=rab;
                 }
-    }
-     public static Ludi findForFam(Ludi lud[], String Fam)
-     {
-         int nom=-1;
-         for (int i = 0; i < lud.length; i++)
-             if (Fam.equals(lud[i].Fam))
-                 nom=i;
-         if (nom!= -1){
-               return lud[nom];
-         }else return (null);
-     }
-        public static void main (String[]args){
-            Scanner sc = new Scanner(System.in, "cp1251");
-            System.out.println("Введите количество людей=> ");
-            int n = sc.nextInt();
-            Ludi[] ludi =setLudi(n);
-            System.out.println("\nХарактеристики людей:");
-            showLudi(ludi);
-            System.out.println("Средний рост мужчин ="+avgRostM(ludi));
-            System.out.println("Средний рост женщин ="+avgRostZH(ludi));
-            int nommax=NomMax(ludi);
-            System.out.println("\nСамый высокий мужчина :");
-            showLudi (ludi[nommax]);
-            SortRost(ludi);
-            System.out.println("\nОтсортированный список по росту:");//
-            showLudi(ludi);
-            System.out.println("Введите фамилию искомых людей=> ");
-            sc.nextLine();
-            String sfam=sc.nextLine();
-            Ludi sfind=findForFam(ludi,sfam);
-            if (sfind!=null)
-            {
-                showLudi(sfind);
-            }else System.out.println("Таких людей в списке нет");
+        System.out.println("\nОтсортированный список по росту:");//
+        for (int i = 0; i < ludi.length; i++) {
+            System.out.println(""+ludi[i].Fam+"\t"+ludi[i].Name +"\t"+ludi[i].Rost+"см"+"\t"+ludi[i].Pol1);
         }
+        sc.nextLine();
+        System.out.println("Введите фамилию искомых людей=> ");
+        String Fam=sc.nextLine();
+        int nom=-1;
+
+        for (int i = 0; i < ludi.length; i++)
+            if (Fam.equalsIgnoreCase(ludi[i].Fam))
+                nom=i;
+        if (nom!= -1)
+        {
+            System.out.println("Такие люди есть в списке. Это "
+                    +ludi[nom].Fam+"\t"+ludi[nom].Name +"\t"+ludi[nom].Rost+"см"+"\t"+ludi[nom].Pol1);
+        }
+        else System.out.println("Таких людей в списке нет");
+
+
+        System.out.println("Введите фамилию человека,у которого нужно отредактировать имя");
+        String Fam1=sc.nextLine();
+        int nom1=-1;
+
+        for (int i = 0; i < ludi.length; i++)
+            if (Fam1.equalsIgnoreCase(ludi[i].Fam))
+                nom1=i;
+        if (nom1!= -1)
+        {
+            System.out.println("Это "
+                    +ludi[nom1].Fam+"\t"+ludi[nom1].Name +"\t"+ludi[nom1].Rost+"см"+"\t"+ludi[nom1].Pol1);
+            System.out.println("Введите измененное имя");
+            ludi[nom1].Name = sc.nextLine();
+            System.out.println("Отредактированные данные "
+                    +ludi[nom1].Fam+"\t"+ludi[nom1].Name +"\t"+ludi[nom1].Rost+"см"+"\t"+ludi[nom1].Pol1);
+        }
+    }
 }
